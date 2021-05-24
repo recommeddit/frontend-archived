4683fb15e23577146d6796fed3d2eedca6452895
SwiftWinds <12981958+SwiftWinds@users.noreply.github.com>
2021-05-23 21:45:14 -0700

    Switch from "on Reddit" to "from Reddit"

--- src/Landing.svelte
+++ src/Landing.svelte
@@ -61,7 +61,7 @@
                 </h1>
                 <p class="text-base text-gray-300 sm:text-xl lg:text-lg xl:text-xl">
                   Our AI-powered recommendation algorithm will search and aggregate top
-                  movie recommendations on Reddit
+                  movie recommendations from Reddit
                 </p>
                 <div class="mt-10 sm:mt-12">
                   <form action="#" class="sm:max-w-xl sm:mx-auto lg:mx-0"
